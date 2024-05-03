d865c659ecb2809b36c1aac2fb30b4f60059a01c
Julian Pompey <jupompey00@gmail.com>
2024-05-03 13:28:15 -0400

    javascript first checkpoint

--- scripts/site.js
+++ scripts/site.js
@@ -2,7 +2,7 @@
 
 //textbox elements
 const numberOfDaysTextBox = document.getElementById("numberOfDaysTextBox");
-//Datebox element
+//Datebox element - not used as of now.
 const pickupDateBox = document.getElementById("pickupDateBox");
 //checkbox elements
 const electronicTollTagCheckbox = document.getElementById("electronicTollTagCheckbox");
@@ -29,26 +29,23 @@ window.onload = function(){
 
 
 function onEstimateTotalCostButtonClicked(){
-    //car rental is $29.99 per day
-    //if under 25 there is a 30% surcharge
-    //taxes already incorporated
-
-    //surcharge = (29.99 + options) * .3 
-    //Total = (surcharge + 29.99 + options ) * numOfDays
 
     //element values
     let numOfDays = Number(numberOfDaysTextBox.value);
     let electronicTollTagCheck = electronicTollTagCheckbox.checked;
     let gpsCheck = gpsCheckbox.checked;
     let roadsideAssistanceCheck = roadsideAssistanceCheckbox.checked;
+
     //I only need to check for one of these realisitcally.
     let under25NoButton = under25NoRadioButton.checked;
     let under25YesButton = under25YesRadioButton.checked;
+
     //arthmetic variables
     let baseRentalCost = 0;
     let surcharge = 0;
     let total = 0;
     let options = 0;
+    let totalOptionsCost = 0;
     //constant
     const CAR_RENTAL_RATE = 29.99;
 
@@ -63,14 +60,19 @@ function onEstimateTotalCostButtonClicked(){
         options += 2.95;
     }
 
-    baseRentalCost = (CAR_RENTAL_RATE + options) * numOfDays
-
+    baseRentalCost = CAR_RENTAL_RATE * numOfDays;
+    totalOptionsCost = options * numOfDays;
     if(under25YesButton){
-        surcharge = (baseRentalCost + options) * .3; 
+
+        surcharge = (baseRentalCost + totalOptionsCost) * .3; 
     }
 
-    total =  baseRentalCost + surcharge;
-    console.log(total);
+    total =  baseRentalCost + totalOptionsCost +  surcharge;
+
+    carRentalCostSpan.innerHTML = baseRentalCost.toFixed(2);
+    optionsCostSpan.innerHTML = totalOptionsCost.toFixed(2);
+    under25SurchargeCostSpan.innerHTML = surcharge.toFixed(2);
+    totalDueSpan.innerHTML = total.toFixed(2);
 }
 
 
