20f7227229e227449c0e23f69a717d5e1abc179c
Julian Pompey <jupompey00@gmail.com>
2024-05-03 12:53:25 -0400

    checkpoint

---
+++ scripts/site.js
@@ -0,0 +1,25 @@
+"use strict"
+
+//textbox elements
+const pickupDateTextBox = document.getElementById("pickupDateTextBox");
+const numberOfDaysTextBox = document.getElementById("numberOfDaysTextBox");
+//checkbox elements
+const electronicTollTagCheckbox = document.getElementById("electronicTollTagCheckbox");
+const gpsCheckbox = document.getElementById("gpsCheckbox");
+const roadsideAssistanceCheckbox = document.getElementById("roadsideAssistanceCheckbox");
+//radio buttons elements
+const under25NoRadioButton = document.getElementById("under25NoRadioButton");
+const under25YesRadioButton = document.getElementById("under25YesRadioButton");
+
+//span elements
+const carRentalCostSpan = document.getElementById("carRentalCostSpan");
+const optionsCostSpan = document.getElementById("optionsCostSpan");
+const under25SurchargeCostSpan = document.getElementById("under25SurchargeCostSpan");
+const totalDueSpan = document.getElementById("totalDueSpan");
+
+//button element
+const estimateTotalCostButton = document.getElementById("estimateTotalCostButton");
+
+window.onload = function(){
+
+}
